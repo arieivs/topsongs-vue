b2dabd157799a220b771e430da929c83942acf4a
Sara Vieira <s.cvieira@sapo.pt>
2022-03-16 17:46:15 +0000

    moving code into card component

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,53 @@
   <div id="app">
     <div class="container">
       <h1>Top 100 Albuns</h1>
-      <div class="card">
-        <a :href="album.url" target="_blank" rel="noreferrer" class="card-link">
-          <div class="card-cover">
-            <img :src="album.coverURL" alt="" />
-            <button class="btn-all btn-prim card-btn-play">Play</button>
-          </div>
-          <h2 class="card-title">{{ album.title }}</h2>
-          <p class="card-artist">{{ album.artist }}</p>
-        </a>
+      <div class="flex-wrap">
+      <cardAlbum v-for="album in albuns" :key="album.id" :album="album"></cardAlbum>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import cardAlbum from './components/cardAlbum.vue'
 
 export default {
   name: 'App',
   components: {
+    cardAlbum
     // register components here later
   },
   data: () => ({
-    album: {
-      title: 'Encanto (Original Motion Picture Soundtrack)',
-      artist: 'Lin-Manuel Miranda, Germaine Franco, Stephanie Beatriz, Olga Merediz & Jessica Darrow',
-      url: 'https://music.apple.com/us/album/encanto-original-motion-picture-soundtrack/1594677532?uo=2',
-      coverURL: 'https://is4-ssl.mzstatic.com/image/thumb/Music126/v4/94/4d/9a/944d9a8d-0549-f537-5706-5b083bd84a7d/21UM1IM38949.rgb.jpg/170x170bb.png'
-    }
+    albuns: [
+      {
+        id: '1594677532',
+        title: 'Encanto (Original Motion Picture Soundtrack)',
+        artist: 'Lin-Manuel Miranda, Germaine Franco, Stephanie Beatriz, Olga Merediz & Jessica Darrow',
+        url: 'https://music.apple.com/us/album/encanto-original-motion-picture-soundtrack/1594677532?uo=2',
+        coverURL: 'https://is4-ssl.mzstatic.com/image/thumb/Music126/v4/94/4d/9a/944d9a8d-0549-f537-5706-5b083bd84a7d/21UM1IM38949.rgb.jpg/170x170bb.png'
+      },
+      {
+        id: '1592745677',
+        title: 'Sing 2 (Original Motion Picture Soundtrack)',
+        artist: 'Various Artists',
+        url: 'https://music.apple.com/us/album/sing-2-original-motion-picture-soundtrack/1592745677?uo=2',
+        coverURL: 'https://is5-ssl.mzstatic.com/image/thumb/Music126/v4/c2/d2/6d/c2d26dcf-7e1b-ed4c-f598-f8dcb42b0f75/21UM1IM35664.rgb.jpg/170x170bb.png'
+      },
+      {
+        id: '1605201128',
+        title: 'LION',
+        artist: 'Elevation Worship',
+        url: 'https://music.apple.com/us/album/lion/1605201128?uo=2',
+        coverURL: 'https://is4-ssl.mzstatic.com/image/thumb/Music116/v4/be/e6/8d/bee68d7f-e72c-8673-d47a-7f65aa0ad5c3/886449816774.jpg/170x170bb.png'
+      },
+      {
+        id: '1611504686',
+        title: 'I NEVER DIE',
+        artist: '(G)I-DLE',
+        url: 'https://music.apple.com/us/album/i-never-die/1611504686?uo=2',
+        coverURL: 'https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/9c/fe/5e/9cfe5eec-89b5-240f-5229-59244360e89d/8804775250422_Cover.jpg/170x170bb.png'
+      },
+    ]
   })
 }
 </script>
---
+++ src/components/cardAlbum.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <a :href="album.url" target="_blank" rel="noreferrer" class="card-link">
+      <div class="card-cover">
+        <img :src="album.coverURL" alt="" />
+        <button class="btn-all btn-prim card-btn-play">Play</button>
+      </div>
+      <h2 class="card-title">{{ album.title }}</h2>
+      <p class="card-artist">{{ album.artist }}</p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cardAlbum',
+  props: {
+    album: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
