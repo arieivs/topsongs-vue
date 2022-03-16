fa29e3f8b90faf884826b82e17c4858c7d9b21d7
Sara Vieira <s.cvieira@sapo.pt>
2022-03-16 15:59:58 +0000

    making it work

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Top 100 Albuns</h1>
+      <div class="card">
+        <a :href="album.url" target="_blank" rel="noreferrer" class="link">
+          <div class="card-cover">
+            <img :src="album.coverURL" alt="" />
+            <button class="btn-all btn-prim play">Play</button>
+          </div>
+          <h2 class="title">{{ album.title }}</h2>
+          <p class="artist">{{ album.artist }}</p>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // register components here later
+  },
+  data: () => ({
+    album: {
+      title: 'Encanto (Original Motion Picture Soundtrack)',
+      artist: 'Lin-Manuel Miranda, Germaine Franco, Stephanie Beatriz, Olga Merediz & Jessica Darrow',
+      url: 'https://music.apple.com/us/album/encanto-original-motion-picture-soundtrack/1594677532?uo=2',
+      coverURL: 'https://is4-ssl.mzstatic.com/image/thumb/Music126/v4/94/4d/9a/944d9a8d-0549-f537-5706-5b083bd84a7d/21UM1IM38949.rgb.jpg/170x170bb.png'
+    }
+  })
 }
 </script>
 
