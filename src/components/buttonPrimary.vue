143b4885717ff60bace8ca4e1641ed9feaa6e7bc
Sara Vieira <s.cvieira@sapo.pt>
2022-03-18 10:58:43 +0000

    created button component just for learning purposes and moved some css to components vue files

---
+++ src/components/buttonPrimary.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="btn-all btn-prim">{{ content }}</button>
+</template>
+
+<script>
+  export default {
+    name: 'buttonPrimary',
+    props: {
+      content: String
+    }
+  }
+</script>
+
+<style>
+  .btn-all {
+    border: none;
+    border-radius: 30px;
+  }
+
+  .btn-prim {
+    padding: 10px 22px;
+    background: linear-gradient(to left, #5717a8, #a340c2);
+    color: #fff;
+  }
+
+  .btn-prim:hover {
+    background: linear-gradient(to left, #6423b6, #bf5ade);
+  }
+</style>
--- src/components/cardAlbum.vue
+++ src/components/cardAlbum.vue
@@ -3,7 +3,7 @@
     <a :href="album.url" target="_blank" rel="noreferrer" class="card-link">
       <div class="card-cover">
         <img :src="album.coverURL" alt="" />
-        <button class="btn-all btn-prim card-btn-play">Play</button>
+        <buttonPrimary :content="'Play'" class="card-btn-play"></buttonPrimary>
       </div>
       <h2 class="card-title">{{ album.title }}</h2>
       <p class="card-artist">{{ album.artist }}</p>
@@ -12,8 +12,13 @@
 </template>
 
 <script>
+import buttonPrimary from './buttonPrimary.vue'
+
 export default {
   name: 'cardAlbum',
+  components: {
+    buttonPrimary
+  },
   props: {
     album: Object
   }
@@ -22,4 +27,57 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .card {
+    max-width: 170px;
+    margin: 15px auto;
+  }
+
+  .card-link {
+    text-decoration: none;
+  }
+
+  .card-cover {
+    position: relative;
+  }
+
+  .card-cover img {
+    border-radius: 5px;
+  }
+
+  .card-link:hover .card-cover img {
+    filter: brightness(50%);
+  }
+
+  .card-btn-play {
+    position: absolute;
+    left: 48px;
+    top: 63px;
+    opacity: 0;
+  }
+
+  .card-link:hover .card-btn-play {
+    opacity: 1;
+    cursor: pointer;
+  }
+
+  .card-title {
+    display: inline-block;
+    font-size: 16px;
+    color: #fff;
+    margin: 5px 0;
+  }
+
+  .card-link:hover .card-title {
+    color: #fff;
+  }
+
+  .card-artist {
+    font-size: 14px;
+    color: #8D91B4;
+    margin: 0;
+  }
+
+  .card-link:hover .card-artist {
+    color: #fff;
+  }
 </style>
